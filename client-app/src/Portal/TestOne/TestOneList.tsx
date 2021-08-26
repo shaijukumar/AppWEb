1f7ca1b2c0be78c7a27355c80dc571627b8fa444
shaijukumar <shaijukumar1980@gmail.com>
2021-08-26 20:16:08 +0400

    26Aug2021

--- client-app/src/Portal/TestOne/TestOneList.tsx
+++ client-app/src/Portal/TestOne/TestOneList.tsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
-import { LinearProgress, TextField  } from '@material-ui/core';
+import { Button, LinearProgress, TextField  } from '@material-ui/core';
 import MaterialTable from 'material-table';
 import { Link } from 'react-router-dom';
+import XLSX from "xlsx";
 
 import { ActionConfig, ApiContext } from '../Api/Api';
 import TableButton from '../../app/common/form/TableButton';
@@ -37,7 +38,7 @@ const TestOneList: React.FC = () => {
 		{title: 'DOB', field: 'DOB', type: "date" }, 	
 		{title: 'Country', field: 'Country'}, 
 		{title: 'Salary', field: 'Salary'},        
-    {title: 'Test1', field: 'Title', 
+    {title: 'TableItemId', field: 'TableItemId', 
     
     editComponent: (value: any, onChange: any,) => {
       debugger;
@@ -63,8 +64,106 @@ const TestOneList: React.FC = () => {
         tooltip: 'Refresh',
         isFreeAction: true, 
         onClick: (event:any) =>{  ApiStore.LoadDataList("TestOneGetNavigationList", setData, setLoading, setError ); },                                     
-    }
+    },
+    {          
+        icon: (values: any) => { 
+            return <Button  variant="contained" color="primary"  component="label" size="small" >
+                        Upload Data<input type="file" multiple={false} onChange={ImportTable}  id="raised-button-file" style={{display: "none",}} />
+                   </Button>  },
+        tooltip: 'Import Table',
+        isFreeAction: true,
+        onClick: (event:any) =>{  },   
+        iconProps: { style: { fontSize: "34px", color: "green", borderRadius:"0%  !important" , backgroundColor:'rosybrown' } },            
+    },
   ];
+
+  const ImportTable = (evt:any) => {
+    // setError('');        
+    var files = evt.target.files; // FileList object
+    var xl2json = new ExcelToJSON();
+    xl2json.parseExcel(files[0]);
+    evt.target.value = null;  
+  };
+
+
+class ExcelToJSON {
+   
+    parseExcel = function(file:any) {
+        
+        var reader = new FileReader();
+
+        reader.onload = function(e:any) {
+            debugger;
+            var data = e.target.result;
+            var workbook = XLSX.read(data, {
+                type: 'binary'
+            });
+
+            // workbook.SheetNames.forEach(function(sheetName) {
+            //     var XL_row_object = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);                                                           
+            //     (XL_row_object as AppNavigation[]).forEach(function (item:AppNavigation) {
+            //         var v = item.Title;
+            //     });   
+            //    return;               
+            // });
+            let formData = new FormData();
+            ApiStore.ExecuteAction(formData, setError);
+
+            var XL_row_object = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);  
+            // ( async() => { 
+            //     await Promise.all(
+            //         var v = item.Title;
+            //         // formData.append('ActionId', actionId.toString() ); 
+            //         // formData.append('Parm1', JSON.stringify(values) );
+            //         // formData.append('ItemId',  values.Id );                      
+            //     );                                                 
+            // })();
+
+            ( async() => { 
+
+                var XL_row_object = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+                var items = XL_row_object as TestOne[];
+
+                var item  = items[0];
+                debugger;
+                if(item.Title) {
+                      let formData = new FormData();
+                      formData.append('ActionId', "18" ); 
+                      formData.append('Parm1', JSON.stringify(item) );  
+                      ApiStore.ExecuteAction(formData, setError).then( (res) => {
+                          console.log("res");
+                      });
+                    }      
+
+                // await Promise.all(
+                //     items.map( async (item:TestOne) => {   
+                //       if(item.Title) {
+                //         let formData = new FormData();
+                //         formData.append('ActionId', "18" ); 
+                //         formData.append('Parm1', JSON.stringify(item) );  
+                //         ApiStore.ExecuteAction(formData, setError).then( (res) => {
+                //             console.log("res");
+                //         });
+                //       }                                            
+                //     })
+                // );
+
+
+
+                alert("Updated");
+
+                
+            })();
+
+        }
+
+        reader.onerror = function(ex) {
+            console.log(ex);
+        };
+  
+        reader.readAsBinaryString(file);
+    }        
+}     
 
   if(loading){
     return <LinearProgress color="secondary"  className="loaderStyle" />     
@@ -93,13 +192,26 @@ const TestOneList: React.FC = () => {
                 }),
 
 
-                onRowDelete: oldData =>
-                new Promise((resolve, reject) => {                  
-                  setTimeout(() => {
-                      debugger;                                     
-                    resolve(true);
-                  }, 10)
-                }),
+                onRowDelete: (oldData:any) =>
+                  new Promise((resolve, reject) => {
+                    
+                    setTimeout(() => {
+                      debugger;
+                        var itemId = Number((oldData as any).Id);                        
+                        let formData = new FormData();
+                        formData.append('ActionId', "21" ); 
+                        formData.append('Parm1', JSON.stringify(oldData) );
+                        formData.append('ItemId',  (oldData as any).Id );
+
+                        ApiStore.ExecuteAction(formData, setError).then( (res) => {            
+                            if(res){
+                                ApiStore.LoadDataList("TestOneGetNavigationList", setData, setLoading, setError );                        
+                            }           
+                        });             
+                      resolve(true);
+                    }, 10)
+                  }),
+
               }}
 
               // cellEditable={{
